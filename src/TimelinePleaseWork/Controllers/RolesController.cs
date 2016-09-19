8b0758e8afccd9e432d68ba03e3e5426d4c78e9d
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-09-18 22:26:28 -0700

    add index and create with Roles controller, only accessible by a logged-in administrator account

---
+++ src/TimelinePleaseWork/Controllers/RolesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using TimelinePleaseWork.Models;
+using TimelinePleaseWork.ViewModels;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Security.Claims;
+using TimelinePleaseWork.Data;
+using Microsoft.AspNetCore.Authorization;
+
+namespace TimelinePleaseWork.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class RolesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RolesController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = _db.Roles;
+            return View(_db.Roles);
+        }
+
+        public IActionResult Create()
+        {
+            var Role = new IdentityRole();
+            return View(Role);
+        }
+        [HttpPost]
+        public IActionResult Create(IdentityRole Role)
+        {
+            _db.Roles.Add(Role);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src/TimelinePleaseWork/ViewModels/RoleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimelinePleaseWork.ViewModels
+{
+    public class RoleViewModel
+    {
+        [Required]
+        [Display(Name = "Role")]
+        public string Role { get; set; }
+    }
+}
