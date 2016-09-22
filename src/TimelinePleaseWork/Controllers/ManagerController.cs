8854e7afb8e39ac364ee037c2173c5fb744f5e59
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-09-21 23:36:48 -0700

    add AJAX request on manager page to display inventory items

--- src/TimelinePleaseWork/Controllers/ManagerController.cs
+++ src/TimelinePleaseWork/Controllers/ManagerController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using TimelinePleaseWork.Models;
+using TimelinePleaseWork.Data;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,6 +14,14 @@ namespace TimelinePleaseWork.Controllers
 {
     public class ManagerController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public ManagerController(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
@@ -21,6 +33,15 @@ namespace TimelinePleaseWork.Controllers
             ViewData["id"] = id;
             ViewData["timeAtLoad"] = DateTime.Now;
             return PartialView();
+        }
+
+        public async Task<IActionResult> Inventory()
+        {
+            var applicationDbContext = _context.InventoryItem.Include(i => i.Item);
+            //ViewData["items"] = new Dictionary<string, Item> { };
+            //var secondDbContext = _context.Items.Include(i => i.Item);
+
+            return PartialView(await applicationDbContext.ToListAsync());
         }
     }
 }
